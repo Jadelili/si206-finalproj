64ea4ddb71df481965a7c4d91f9755e10da08731
李潇怡 <lixiaoyi@lixiaoyideMacBook-Pro.local>
2023-04-20 01:34:07 -0400

    add sunlight clean data

--- weather_health_load.py
+++ weather_health_load.py
@@ -110,12 +110,12 @@ def process_health_data(healthfile_r, healthfile, three_city_d):
                 else:
                     hd["no_leis_phy_act"] = "null"
 
-                if h_city["placename"] not in city_d.keys():
+                if h_city["stateabbr"] == "WV" and h_city["placename"] == "Charleston":
+                    city_d["Charleston_WV"] = hd
+                elif h_city["placename"] not in city_d.keys():
                     city_d[h_city["placename"]] = hd
                 elif h_city["stateabbr"] == "ME" and h_city["placename"] == "Portland":
                     city_d["Portland"] = hd
-                elif h_city["stateabbr"] == "WV" and h_city["placename"] == "Charleston":
-                    city_d["Charleston"] = hd
                 elif h_city["stateabbr"] == "MD" and h_city["placename"] == "Columbia":
                     city_d["Columbia"] = hd
 
@@ -222,12 +222,12 @@ def process_weather_data(weatherfile_r, weatherfile):
                     d["humidity_medium"] = hum_avg
                     d["clouds_medium"] = c_avg
 
+                    if state == "WV" and city == "Charleston":
+                        city_d["Charleston_WV"]= d
                     if city not in city_d.keys():
                         city_d[city] = d
                     elif state == "ME" and city == "Portland":
                         city_d["Portland"] = d
-                    elif state == "WV" and city == "Charleston":
-                        city_d["Charleston"]= d
                     elif state == "MD" and city == "Columbia":
                         city_d["Columbia"] = d
                 
@@ -364,13 +364,91 @@ def make_state_table(filename, cur, conn):
     conn.commit()
 
 
+def cache_sun_data(three_city_d, sunfile):
+    sun_d = {}
+    base = "https://archive-api.open-meteo.com/v1/archive"
+    for state in three_city_d:
+        d = {}
+        for city in three_city_d[state]:
+            dic = {"latitude":three_city_d[state][city][0], "longitude":three_city_d[state][city][1], "start_date":"2020-11-01", "end_date":"2021-03-31", 
+            "daily":"temperature_2m_mean,apparent_temperature_mean,sunrise,sunset,shortwave_radiation_sum,precipitation_sum,rain_sum,snowfall_sum,precipitation_hours",
+            "timezone":"America/Los_Angeles","temperature_unit":"fahrenheit","precipitation_unit":"inch"}
+            result = get_api(base, dic)
+            d[city] = result
+            if state not in sun_d.keys():
+                sun_d[state] = d
+            else:
+                sun_d[state].update(d)
+    write_json(sunfile, sun_d)
+
+
+def process_sun_data(sunfile_r, sunfile):
+    sun_r = load_json(sunfile_r)
+    lst = []
+    sun_d = {}
+    for state in sun_r:
+        city_d = {}
+        for city in sun_r[state]:
+            d = {}
+            day_num = len(sun_r[state][city]["daily"]["time"])
+
+            duration = 0
+            temp = 0
+            app_temp = 0
+            rad = 0
+            prec = 0
+            rain = 0
+            snow = 0
+            prec_h = 0
+            for i in range(day_num):
+                temp += sun_r[state][city]["daily"]["temperature_2m_mean"][i]
+                app_temp += sun_r[state][city]["daily"]["apparent_temperature_mean"][i]
+                rad += sun_r[state][city]["daily"]["shortwave_radiation_sum"][i]
+                prec += sun_r[state][city]["daily"]["precipitation_sum"][i]
+                rain += sun_r[state][city]["daily"]["rain_sum"][i]
+                snow += sun_r[state][city]["daily"]["snowfall_sum"][i]
+                prec_h += sun_r[state][city]["daily"]["precipitation_hours"][i]
+                # print(sun_r[state][city]["daily"]["sunset"][i], sun_r[state][city]["daily"]["sunrise"][i])
+                sunrise = int(sun_r[state][city]["daily"]["sunrise"][i][-4:-3]) * 60 + int(sun_r[state][city]["daily"]["sunrise"][i][-2:])
+                sunset = int(sun_r[state][city]["daily"]["sunset"][i][-5:-3]) * 60 + int(sun_r[state][city]["daily"]["sunset"][i][-2:])
+                duration_day = sunset - sunrise
+                duration += duration_day
+            
+            d["temp"] = temp / day_num
+            d["app_temp"] = app_temp / day_num
+            d["rad"] = rad / day_num
+            d["prec"] = prec / day_num
+            d["rain"] = rain / day_num
+            d["snow"] = snow / day_num
+            d["prec_hours"] = prec_h / day_num
+            d["sunlight_hours"] = duration / day_num
+
+            if state == "WV" and city == "Charleston":
+                city_d["Charleston_WV"] = d
+            elif city not in city_d.keys():
+                city_d[city] = d
+            elif state == "ME" and city == "Portland":
+                city_d["Portland"] = d
+            elif state == "MD" and city == "Columbia":
+                city_d["Columbia"] = d
+        
+        if state not in list(sun_d.keys()):
+            sun_d[state] = city_d
+        else:
+            sun_d[state].update(city_d)
+    write_json(sunfile, sun_d)
+    return sun_d
+
+
 def main():
     three_city_d = get_lat("health_data_r.json", "coordinate.json")
     ###cache_health_data("health_data_r.json")
     ###cache_weather_data(three_city_d, "weather_data_raw.json")
     ###cache_elevation_data(three_city_d, "elevation_data.json")
+    ### cache_sun_data(three_city_d, "sun_data_r.json")
     process_weather_data("weather_data_raw.json", "weather_data.json")
     process_health_data("health_data_r.json", "health_data.json", three_city_d)
+    process_sun_data("sun_data_r.json", "sun_data.json")
     cur, conn = open_database("Mental_health.db")
     make_state_table("weather_data.json", cur, conn)
     make_weather_table("weather_data.json", cur, conn)
